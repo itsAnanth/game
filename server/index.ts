71db7f294cdc9b6ea278be8ab51038ef03ee3be4
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-12 16:54:07 +0530

    chore: server cleanup

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,9 @@
 import Server from './components/WsServer';
+import { App } from 'uWebSockets.js';
 
-
-const server = new Server({ port: 3000 });
+const app = App();
+const server = new Server({ app, port: 8000 });
 server.start();
+
+
+setInterval(() => server.update(), 50);

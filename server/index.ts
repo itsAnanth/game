ad2056c660bd8e94c7d50599d736324d4d47076c
EJBEAN2op <danonymousarmy@gmail.com>
2022-05-13 17:48:16 +0530

    feat: variable server port

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import Server from './components/WsServer';
 import { App } from 'uWebSockets.js';
 
 const app = App();
-const server = new Server({ app, port: 8000 });
+const server = new Server({ app, port: Number(process.env.SERVER_PORT) || 8000 });
 server.start();
 
 
